94f89a0831bfc952f5fe3508620595a0894c159d
João Paulo Rodrigues <joaopaulo.dev@hotmail.com>
2024-08-30 11:33:44 -0300

    deploy final

--- app_teste/settings.py
+++ app_teste/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,11 +78,32 @@ WSGI_APPLICATION = 'app_teste.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# 👇 2. Update the below python dict DATABASES
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "db_app_teste",
+#         "USER": "joaopaulo",
+#         "PASSWORD": "123@456#",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
+
+# Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://app_teste_user:H0YpvqOaNGMYDmuQDOmSmRYVlAvLs1y0@dpg-cr8tbitsvqrc739g2k4g-a/app_teste',        
+        conn_max_age=600
+    )
 }
 
 
