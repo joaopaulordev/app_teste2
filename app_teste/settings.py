d8e51d7937a225199befae302b0ae7f6b6707ebe
João Paulo Rodrigues <joaopaulo.dev@hotmail.com>
2024-08-30 11:40:34 -0300

    deploy pronto

--- app_teste/settings.py
+++ app_teste/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'app_teste.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://app_teste_user:H0YpvqOaNGMYDmuQDOmSmRYVlAvLs1y0@dpg-cr8tbitsvqrc739g2k4g-a/app_teste',        
+        default='postgresql://db_teste_p67p_user:SYpZUWN4jA51P8FHNWJx4Q6JvUP1gX2c@dpg-cr8tj7qj1k6c73f900kg-a/db_teste_p67p',        
         conn_max_age=600
     )
 }
